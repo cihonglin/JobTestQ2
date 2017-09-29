f6162872c1ab088b62af7700888470c118d796d2
cihonglin <cihonglin@gmail.com>
2017-09-29 09:53:36 +0900

    edit var

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 import re, sys, getopt
 
-def replace_var(inputStr,inputVariable):
+def replaceVar(inputStr,inputVariable):
 	var_list = inputVariable.split(',')
 	new_str = inputStr
 	for var_x in var_list:
@@ -16,7 +16,7 @@ def cale(x,y,c):
 	opration={'+':x+y,'-':x-y,'*':x*y}
 	return opration.get(c,'error')
 
-def do_cale(formulaStr):
+def doCale(formulaStr):
 	num_list = re.findall(r"\-?\d+\.?\d*",formulaStr)
 	cale_mark = re.findall(r"[^0-9]",formulaStr)
 
@@ -27,10 +27,10 @@ def do_cale(formulaStr):
 
 	return result
 
-def renew_formula(formulaStr):
+def renewFormula(formulaStr):
 	return re.sub('\+\-','-',re.sub('\-\-', '+',formulaStr))
 
-def init_pare(inputStr):
+def initPare(inputStr):
 	pare_left,pare_right = '(',')'
 	pare_count = inputStr.count(pare_left) #counting '(' nums 
 	new_string = inputStr
@@ -40,7 +40,7 @@ def init_pare(inputStr):
 			pare_start = new_string.find(pare_left)
 			sub_formula_str = new_string[pare_start + 1:pare_end]
 
-			cale_result = str(do_cale(sub_formula_str))
+			cale_result = str(doCale(sub_formula_str))
 			new_string = new_string[:pare_start] + cale_result + new_string[pare_end + 1:] 
 
 			pare_count -= 1
@@ -51,17 +51,17 @@ def init_pare(inputStr):
 """
 do mulie cate
 """
-def init_cale(inputStr,pare):
+def initCale(inputStr,pare):
 	pare_count = inputStr.count(pare) #counting nums 
 	if pare_count > 0:
 		pattern_str = re.compile(r"\-?\d*"+ re.escape(pare) + "\-?\d*")
 		cale_list = re.findall(pattern_str,inputStr)
 		cale_result_list = [];
 		for mulit_cale in cale_list:
-			cale_result_list.append(do_cale(mulit_cale))
+			cale_result_list.append(doCale(mulit_cale))
 			pare_start = inputStr.find(mulit_cale)
 
-			cale_result = str(do_cale(mulit_cale))
+			cale_result = str(doCale(mulit_cale))
 			inputStr = inputStr[:pare_start] + cale_result + inputStr[pare_start + len(mulit_cale):] 
 		return inputStr
 	else:
@@ -69,7 +69,7 @@ def init_cale(inputStr,pare):
 """
 do add & minus cale
 """
-def init_add_minus(inputStr):
+def initAddMinus(inputStr):
 	cale_list = re.findall('\+|\-',inputStr)
 	pattern_str = re.compile(r"\-?\d*\+|\-\-?\d*")
 	if(inputStr[0] == '-'):
@@ -114,13 +114,13 @@ def get_file(argv):
 
 
 def main(formula,inputVariable):
-	replace_formula = replace_var(formula,inputVariable); #replace var 
+	replace_formula = replaceVar(formula,inputVariable); #replace var 
 	print "replace_formula => " + replace_formula + "\n"
-	renew_formula_result = renew_formula(init_pare(replace_formula))
+	renew_formula_result = renewFormula(initPare(replace_formula))
 	print "renew_para_result => " + renew_formula_result + "\n"
-	mulit_result =  renew_formula(init_cale(renew_formula_result,'*'))
+	mulit_result =  renewFormula(initCale(renew_formula_result,'*'))
 	print "do_plus_result => " + mulit_result + "\n"
-	add_result = init_add_minus(mulit_result)
+	add_result = initAddMinus(mulit_result)
 	print "add_minus_result => " + str(add_result) + "\n"
 	return add_result
 
