78868c0f212a89e9e3e21ecc98c9e2086cc801a6
cihonglin <cihonglin@gmail.com>
2017-10-02 21:51:59 +0900

    多層括號處理

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 # encoding: utf-8
 import re, sys, getopt
 
-
-
 def replaceVar(inputStr,inputVariable):
 	var_list = inputVariable.split(',')
 	new_str = inputStr
@@ -20,38 +18,14 @@ def cale(x,y,c):
 
 def doCalculate(x,y,c):
 	
-	#print "x="+ str(x) 
-	#print "y="+ str(y)
-
-	if str(x).isdigit() and str(y).isdigit():
+	if (str(x).lstrip('-').isdigit()) and (str(y).lstrip('-').isdigit()):
 		op1 = int(x)
 		op2 = int(y)
-		result = cale(op1,op2,str(c))		
+		result = cale(op1,op2,str(c))
 	else:
 		result = str(x)+c+str(y)
-	return result
 
-
-def doCale(formulaStr):
-	num_list = re.findall(r"\-?\d+\.?\d*",formulaStr)
-	cale_mark = re.findall(r"[^0-9]",formulaStr)
-
-	print cale_mark
-
-	if(len(cale_mark) > 1 and cale_mark[0] == '-'):
-		mark = cale_mark[1]
-		x = int(num_list[0])
-	else:
-		mark = cale_mark[0]
-		x = int(num_list[0])
-	
-
-	#mark = cale_mark[0]
-	
-	y = int(num_list[1])
-	result = cale(x,y,mark)
-
-	#print num_list[0] + mark + num_list[1] +"="+str(result)
+	#print result
 	return result
 
 def renewFormula(formulaStr):
@@ -60,21 +34,38 @@ def renewFormula(formulaStr):
 
 def initPare(inputStr):
 	pare_left,pare_right = '(',')'
-	pare_count = inputStr.count(pare_left) #counting '(' nums 
-	new_string = inputStr
+	pare_count = inputStr.count(pare_left) 
+	new_string = pop_string = ''
+	dump_list = []
+	
 	if pare_count > 0:
-		while pare_count > 0:
-			pare_end = new_string.find(pare_right)
-			pare_start = new_string.find(pare_left)
-			sub_formula_str = new_string[pare_start + 1:pare_end]
-			cale_result = str(doMath(sub_formula_str))
-			new_string = new_string[:pare_start] + cale_result + new_string[pare_end + 1:] 
+		for char in inputStr:
+			if char != pare_right :
+				dump_list.append(char)
+				#print " << [ " + char + " ]  dump_list : " + "".join(dump_list)
+			else:
+				if char == pare_right :
+					#pop_string = dump_list.pop()
+					
+					while len(dump_list) > 0 :
+						#print "dump_list : " + "".join(dump_list)
+						pop_string = dump_list.pop()
+						#print " >> [ " + pop_string + " ]  dump_list : " + "".join(dump_list)
+						if pop_string == pare_left:
+							break;
+						else:
+							new_string = pop_string + new_string
+							
+				cale_result = str(doMath(new_string))
+				dump_list.append(cale_result)
+				#print " << [ " + char + " ]  dump_list : " + "".join(dump_list)
+				#print "pop dump_list:" + new_string + " = " + cale_result
+				new_string = ''
 
-			pare_count -= 1
-		return new_string
+		return renewFormula("".join(dump_list))
 
 	else:
-		return pare_count
+		return inputStr
 """
 do mulie cate
 """
@@ -117,6 +108,7 @@ def initAddMinus(inputStr):
 	#print cale_list
 	#print count_list	
 
+	result = inputStr
 
 	#print len(cale_list)
 	while len(cale_list) != 0:
@@ -126,20 +118,20 @@ def initAddMinus(inputStr):
 		op2 = count_list.pop()
 		m = cale_list.pop()
 
-		r = doCalculate(op1,op2,m)
-		count_list.append(r)
+		result = doCalculate(op1,op2,m)
+		count_list.append(result)
 
-		#print str(op1)+m+str(op2)+"="+str(r)
+		#print str(op1)+m+str(op2)+"="+str(result)
 
-	return r
-
-
+	return result
 
 def doMath(renewFormulaResult):
 	mulit_result =  renewFormula(initCale(renewFormulaResult,'*'))
 	#print "do_plus_result => " + mulit_result + "\n"
 	add_result = initAddMinus(mulit_result)
 	#print "add_minus_result => " + str(add_result) + "\n"
+	#if(add_result[0] == '0'):
+	#	add_result = add_result[1:]
 	return add_result
 
 def openFile(fileName):
@@ -152,46 +144,47 @@ def openFile(fileName):
 	else:
 		print fileName+" not exists"
 
-
-
-
-
 print "----- Question 2 Start-----"
 
-string = raw_input("input (1-9)：");
+option = raw_input("input (0-9)：");
 
-if string.isdigit() and int(string) > 0 and int(string) < 10:
-	filename = "input" + string + ".txt"
+if option.isdigit() and int(option) >= 0 and int(option) < 10:
+	filename = "input" + option + ".txt"
 	print "input file name : "+filename
+
+	question_str = openFile("input/"+filename)
+
+	split_list = question_str.split()
+
+	split_list.reverse()
+
+	formula = split_list.pop()
+
+	split_list.reverse()
+
+	#print formula_string
+
+	print "\nformula => " + formula + "\n"
+
+	for input_variable in split_list :
+		print "**********************************"
+		#print formula + "\n"
+		print input_variable
+		replace_formula = replaceVar(formula,input_variable); #replace var 
+		print "replace_formula => " + replace_formula + "\n"
+		init_pare = initPare(replace_formula)
+		#if option == '3':
+		#	print init_pare
+	#		break
+		renew_formula_result = renewFormula(init_pare)
+		#print renew_formula_result
+		print doMath(init_pare)
+		print "**********************************\n"
+
+	print "\n----- Question 2 End-----"
+
 else:
 	print "out of range"
-
-question_str = openFile("input/"+filename)
-
-split_list = question_str.split()
-
-split_list.reverse()
-
-formula = split_list.pop()
-
-split_list.reverse()
-
-#print formula_string
-
-print "\nformula => " + formula + "\n"
-
-for input_variable in split_list :
-	print "**********************************"
-	#print formula + "\n"
-	print input_variable
-	replace_formula = replaceVar(formula,input_variable); #replace var 
-	#print "replace_formula => " + replace_formula + "\n"
-	renew_formula_result = renewFormula(initPare(replace_formula))
-	#print renew_formula_result
-	print doMath(renew_formula_result)
-	#print "**********************************\n"
-
-print "\n----- Question 2 End-----"
 
 
 
